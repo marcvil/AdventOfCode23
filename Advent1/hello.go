f114667d88904b9c1eeb8bdb28483f858ef039fb
Marcvil <marcvilabascara@gmail.com>
2023-12-06 00:43:36 +0100

    I think go doesnt have regex positiv lookaheads...

--- Advent1/hello.go
+++ Advent1/hello.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
+	"regexp"
 	"strconv"
-	"unicode"
 )
 
 func main() {
@@ -16,33 +17,41 @@ func main() {
 	defer file.Close()
 
 	var slice []string
-	slice = make([]string, 0, 200)
+	slice = make([]string, 0, 2000)
 	scanner := bufio.NewScanner(file)
-	// optionally, resize scanner's capacity for lines over 64K, see next example
+	// optionall	y, resize scanner's capacity for lines over 64K, see next example
 	for scanner.Scan() {
 		slice = append(slice, scanner.Text())
+	}
+
+	regexPattern := regexp.MustCompile(`(?:(one|two|three|four|five|six|seven|eight|nine))`)
+
+	values := map[string]string{
+		"one":   "1",
+		"two":   "2",
+		"three": "3",
+		"four":  "4",
+		"five":  "5",
+		"six":   "6",
+		"seven": "7",
+		"eight": "8",
+		"nine":  "9",
 	}
 
 	var sum int
 
 	for i := 0; i < len(slice); i++ {
-		var numberString string
-		word := []rune(slice[i])
-		for j := 0; j < len(word); j++ {
-			if unicode.IsDigit(word[j]) {
-				log.Printf(string(word[j]))
-				numberString += string(word[j])
-				break
-			}
-		}
+		matches := regexPattern.FindAllString(slice[i], -1)
 
-		for j := len(word) - 1; j >= 0; j-- {
-			if unicode.IsDigit(word[j]) {
-				log.Printf(string(word[j]))
-				numberString += string(word[j])
-				break
-			}
-		}
+		var first string
+		var last string
+		var numberString string
+
+		first = getValue(matches[0], values)
+		last = getValue(matches[len(matches)-1], values)
+		numberString = first + last
+
+		fmt.Println(numberString)
 
 		int1, err := strconv.Atoi(numberString)
 		if err != nil {
@@ -52,4 +61,12 @@ func main() {
 	}
 
 	log.Printf("Sum is: %d", sum)
+
+}
+
+func getValue(str string, dict map[string]string) string {
+	if len(str) > 1 {
+		return dict[str]
+	}
+	return str
 }
